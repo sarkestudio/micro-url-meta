11b7513fb8fee3f8492d63d8c370e34dad629480
Mirza Brunjadze <brunjadzem@gmail.com>
2017-08-09 14:36:41 +0400

    Performance optimization

--- html.js
+++ html.js
@@ -1,9 +1,14 @@
 const fetch = require('node-fetch');
+const body_matcher = /<body\s?\w*>(.|\r|\n)*<\/body>/;
 
 async function fetch_html(url) {
   try {
     const response = await fetch(url);
-    return response.text();
+    const html = await response.text();
+
+    // Strip <body> tag from html,
+    // as we are only interested in meta tags
+    return html.replace(body_matcher, '');
   } catch (e) {
     return '';
   }
