0bba148c37870f088af7a4fcccf6962f2108bf38
Mirza Brunjadze <brunjadzem@gmail.com>
2017-08-30 15:46:39 +0400

    ocd

--- image.js
+++ image.js
@@ -1,13 +1,12 @@
 const probe = require('probe-image-size');
-
-const is_image_pattern = /\.(jpe?g|gif|png|bmp|tiff|svg)(\?.*)?$/;
+const is_image_matcher = /\.(jpe?g|gif|png|bmp|tiff|svg)(\?.*)?$/;
 
 function fetch_image(url) {
   return probe(url, { timeout: 5000 });
 }
 
 function is_image(url) {
-  return is_image_pattern.test(url.toLowerCase());
+  return is_image_matcher.test(url.toLowerCase());
 }
 
 module.exports = {
